7499d8c11899c8ed2a2c6e6ca1e92b5752cf8d86
IshaGeurtsen <ishageurtsen@aryzon.world>
2024-12-03 17:23:48 +0100

    make live build generic to the day

--- 2024/runner/live_build.py
+++ 2024/runner/live_build.py
@@ -9,25 +9,22 @@ import typing
 
 cwd = pathlib.Path.cwd()
 src = cwd / "app/Main.hs"
-cabal = cwd/"day03.cabal"
-build = cwd / "dist-newstyle/build/x86_64-windows/ghc-9.2.1/day03-0.1.0.0/x/day03/build/day03/day03.exe"
+cabal = next(cwd.glob("day*.cabal"))
 last_build_attempt: typing.Optional[datetime.datetime] = None
 
 try:
     while True:
         most_recent_change = max(src.stat().st_mtime, cabal.stat().st_mtime)
-        build_time = build.stat().st_mtime if build.exists() else None
-        if build_time is None or most_recent_change > build_time and (last_build_attempt is None or most_recent_change > last_build_attempt):
+        if (last_build_attempt is None or most_recent_change > last_build_attempt):
             print("\n---")
             try:
                 print(subprocess.check_output(["cabal", "build"]).decode())
             except subprocess.CalledProcessError:
                 traceback.print_exc(0)
             else:
-                os.utime(build)
-                print(datetime.datetime.fromtimestamp(build.stat().st_mtime).time())
+                print(datetime.datetime.now().time())
                 try:
-                    print(subprocess.check_output([str(build)]).decode())
+                    print(subprocess.check_output(["cabal", "run"]).decode())
                 except subprocess.CalledProcessError:
                     traceback.print_exc(0)
             finally:
